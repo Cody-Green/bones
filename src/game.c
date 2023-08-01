827c8ca36445a22551cfc1ea99ca1c19930fd1d9
Cody Green <cody.william.green@gmai.com>
2023-08-01 13:07:54 -0700

    Made changes to the loop and the WM_PAINT case

--- src/game.c
+++ src/game.c
@@ -58,9 +58,9 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR pCmdLin
             DispatchMessage(&msg);
         }
         else {
-           
-            
             Sleep(1000);
+            currentBackgroundColor = (currentBackgroundColor == RGB(0, 0, 0)) ? RGB(255, 255, 255) : RGB(0, 0, 0);
+            InvalidateRect(hwnd, NULL, TRUE);
         }
 
         
@@ -84,7 +84,6 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hwnd, &ps);
             HBRUSH brush = CreateSolidBrush(currentBackgroundColor);
-           
             FillRect(hdc, &ps.rcPaint, brush);
             DeleteObject(brush);
             EndPaint(hwnd, &ps);
